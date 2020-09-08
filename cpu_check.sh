c204dda7c74686d31229e39fd23084ba36f5946e
samuel <samybimbo@gmail.com>
2020-09-07 20:13:32 -0400

    new file

---
+++ cpu_check.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+
+
+#Description: Checking the number of CPU's
+
+#Author: Samuel
+
+#Date: 07/2020
+
+NUM_CPU= `nproc`
+
+
+
+if [ ${NUM_CPU} LT2 ]
+then
+
+echo "This server does not have enough cpu's"
+
+else
+
+if [ $ -gt 2 ]  
+
+
+
+echo "This server meets the requirements"
+fi
+
