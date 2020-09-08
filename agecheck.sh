81158ae663730e23f504bafff78cf7d5ae095cdd
samuel <samybimbo@gmail.com>
2020-09-07 20:01:51 -0400

    new file

---
+++ agecheck.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+
+
+#Description: Age check at liquor store
+
+#Author: Samuel
+#Date: 07/2020
+echo "what year were you born? "
+
+read Y
+
+echo "Please wait while we are checking few stuff"
+
+sleep 3
+
+if
+
+[ $Y -gt 1999 ]
+
+then
+
+echo "Go bring you parents"
+
+else
+
+echo "Welcome to the store, please visit ailse 13"
+
+fi
+
